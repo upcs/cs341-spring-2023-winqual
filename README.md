c410800ed8b7e7e1450ec59f014424f70238731a
Chase Ohmstede <aceohmstede@gmail.com>
2023-05-02 00:04:34 -0700

    updating main-fix w commit history (p1)
    this is The BMOC -- my nerves are so shot I'm sweating through my teeth rn

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # cs341-spring-2023-winqual
 
 WinQual 2.0 Application Development
-  
+
 Created by: Margo Brown, Selena Li, Thomas Kone, Emily Do, and Chase Ohmstede
 
-Link to Webpage: https://upcs.github.io/cs341-spring-2023-winqual/winqual/public/index.html#
+Link to Webpage: https://upcs.github.io/cs341-spring-2023-winqual/winqual/public/index.html
 
 # Acceptance Test
 
@@ -12,39 +12,43 @@ Test Case: Removing and item from a dropbox
 
 Description: The user drags a anion and/or cation to the trash can icon to delete the item.
 
-### Test Steps: 
-  1. Open the web page.
-  2. Click and hold on an image to drag it.
-  3. Drag the image to its corresponding dropbox (either cation or anion) and let go of mouse.
-     - Or drag the image directly onto the trash can icon. 
-  4. Drag the image onto the trash can icon.
-  
+### Test Steps:
+
+1. Open the web page.
+2. Click and hold on an image to drag it.
+3. Drag the image to its corresponding dropbox (either cation or anion) and let go of mouse.
+   - Or drag the image directly onto the trash can icon.
+4. Drag the image onto the trash can icon.
+
 ### Expected Results:
-  1. The web page should open and display the main page.
-  2. The image should be able to move in correspondence to the mouse. 
-  3. The image should be fit into the box that it is placed at.
-     - The image should disappear.
-  4. The image should disappear.
+
+1. The web page should open and display the main page.
+2. The image should be able to move in correspondence to the mouse.
+3. The image should be fit into the box that it is placed at.
+   - The image should disappear.
+4. The image should disappear.
 
 If all expected results are met, the acceptance test is considered successful.
 
 # Completed Features
 
 **1. Drag and drop**
-   - Click and hold on an image to drag to its corresponding box (cation or anion).
-   - Dragging a second NaOH into the anion box results in NaOH in excess.
-     
+
+- Click and hold on an image to drag to its corresponding box (cation or anion).
+- Dragging a second NaOH into the anion box results in NaOH in excess.
+
 **2. Check Solution button**
-  - If a "1 Mystery Cation" is placed into the cation box, after experimenting with various anions to figure out what Cation it is, click this button. A popup will open up with 6 options of what the Mystery Cation may be. If selected correctly, the box will highlight green and red if not.
-  
-**3. Show Mixture button**
-  - After at least 1 image is placed into each box, click this button to see its final mixture in the larger box below.
 
-**4. Reset Unknown button**
-  - To reset all placed images on the webpage, click this button to clear the page and start over.
+- If a "1 Mystery Cation" is placed into the cation box, after experimenting with various anions to figure out what Cation it is, click this button. A popup will open up with 6 options of what the Mystery Cation may be. If selected correctly, the box will highlight green and red if not.
 
-**5. Help button**
-  - Isolates each component of the webpage to explain what each does.
+**3. Reset Unknown button**
 
-**6. Trash**
-  - Drag unwanted images over the trash icon to delete.
+- To reset all placed images on the webpage, click this button to clear the page and start over.
+
+**4. Help button**
+
+- Isolates each component of the webpage to explain what each does.
+
+**5. Trash**
+
+- Drag unwanted images over the trash icon to delete.
--- winqual/public/main.js
+++ winqual/public/main.js
@@ -12,76 +12,96 @@ function drop(ev) {
   var dropBox = ev.target;
   var img = document.getElementById(ev.dataTransfer.getData("text"));
 
+  if (dropBox.id.includes("_copy")) {
+    //god I hate this, basically its setting the img as the target, this if statement corrects it to be the dropBox
+    if (dropBox.id.includes("A")) {
+      //we are at anion dropBox
+      dropBox = document.getElementById("anion");
+      //console.log("Checkpoint 1");
+    } else {
+      //we are at cation dropBox
+      dropBox = document.getElementById("cation");
+    }
+  }
+
   if (dropBox.classList.item(1) == img.classList.item(1)) {
+    //make sure img type matches dropBox type, anion or cation
     var numElems = dropBox.getElementsByTagName("*").length;
-    /* make sure box is empty */
+
     if (numElems == 0) {
+      //dropBox is empty
       var imgCopy = img.cloneNode(true);
       imgCopy.id += "_copy";
       dropBox.appendChild(imgCopy);
+    } else if (numElems == 1) {
+      //there is 1 img already in dropBox
+      const existingImg = dropBox.firstChild;
+      //console.log("last child: " + dropBox.lastChild.id);
+      //console.log("first child: " + dropBox.firstChild.id);
 
-      /* check for NaOH being added in xs */
-    } 
-    else if (numElems == 1 && img.id == "A2") {
-      const existingImg = document.getElementById("A2_copy");
-      
-      if (existingImg.id !== null) {
-        /* currently works correctly on the front end but doesn't delete the old image */
-        existingImg.src = "images/solutions_IMGS/9_unknown.JPG";
+      if (existingImg.id.includes("A2") && img.id.includes("A2")) {
+        //if existingImg and img are both NaOH (or A2) then replace with NaOH XS
         existingImg.id = "A5_copy";
-        console.log("bitch it worked");
+        existingImg.src = "imagessolutions_IMGS/9_unknown.jpg";
         const rm = document.getElementById("A2_copy");
-        rm.remove();
+        //rm.remove();
+        //console.log("Are you activating?");
+        return;
+      } else {
+        //all other existingImgs just get replaced
+        const tempId = existingImg.id;
+        existingImg.id = img.id + "_copy";
+        existingImg.src = img.src;
+        const rm = document.getElementById(tempId);
+        //rm.remove();
+        //console.log("what is supposed to go here");
       }
     }
-
   } else if (dropBox.id.includes("trash") && img.id.includes("_copy")) {
     img.remove();
+  } else {
+    return;
   }
-  else{return;}
 }
-
-
 
 function help_click() {
   alert("We are still working on implementing the help page :)");
-  if (ev.target.id == trash) document.getElementById(target.id).remove(); 
+  if (ev.target.id == trash) document.getElementById(target.id).remove();
 }
 
 /* "Check Solutions" popup */
-$ = function(id) {
+$ = function (id) {
   return document.getElementById(id);
-}
+};
 
-/* 
-* Opens the popup on click ("Check Solutions", "Help", and "Credits" button) 
-* Closes popup on following click
-*/
-var show = function(id) {
+/*
+ * Opens the popup on click ("Check Solutions", "Help", and "Credits" button)
+ * Closes popup on following click
+ */
+var show = function (id) {
   var element = document.getElementById(id);
   if (element.style.display == "none") {
     element.style.display = "block";
-  }
-  else {
+  } else {
     element.style.display = "none";
   }
-}
+};
 
 /* Closes the popup on click ("Close" button) */
-var hide = function(id) {
-    $(id).style.display = 'none';
-}
+var hide = function (id) {
+  $(id).style.display = "none";
+};
 /* Closes the popup whenever you click anywhere else on the screen  */
-window.onload = function() {
-  document.onclick = function(e) {
-    if (e.target.id == 'solutionsPopup') {
-      solutionsPopup.style.display = 'none';
+window.onload = function () {
+  document.onclick = function (e) {
+    if (e.target.id == "solutionsPopup") {
+      solutionsPopup.style.display = "none";
     }
-    if (e.target.id == 'overlay') {
-      overlay.style.display = 'none';
+    if (e.target.id == "overlay") {
+      overlay.style.display = "none";
     }
-    if (e.target.id == 'credits') {
-      credits.style.display = 'none';
+    if (e.target.id == "credits") {
+      credits.style.display = "none";
     }
-  }
-}
+  };
+};
